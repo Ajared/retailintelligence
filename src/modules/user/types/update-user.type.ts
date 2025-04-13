c8bd0432b79b72a9ccdbb8edd77f4f8d771091fc
Prudent Bird <prudentbird@gmail.com>
2025-04-13 23:21:30 +0100

    Revert "feat(user): implement user management module and service"

--- src/modules/user/types/update-user.type.ts
+++
@@ -1,9 +0,0 @@
-import { UserInterface } from './user.interface';
-import UpdateGenericRecord from '~/types/generic/update-record.type';
-type UpdateUserRecordPayload = Partial<UserInterface>;
-type UpdateUserRecordOptions = UpdateGenericRecord<
-  UpdateUserRecordPayload,
-  Record<string, unknown>
->;
-
-export default UpdateUserRecordOptions;
