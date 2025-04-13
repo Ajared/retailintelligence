3c7c3204313a4f6a630847b021f826eafb03fb08
Prudent Bird <prudentbird@gmail.com>
2025-04-13 23:21:30 +0100

    Revert "feat(user): implement user management module and service"

--- src/modules/user/types/create-user.type.ts
+++
@@ -1,7 +0,0 @@
-import { UserInterface } from './user.interface';
-import CreateGenericRecord from '~/types/generic/create-record.type';
-
-type CreateUserRecordPayload = Partial<UserInterface>;
-type CreateUserRecordOptions = CreateGenericRecord<CreateUserRecordPayload>;
-
-export default CreateUserRecordOptions;
