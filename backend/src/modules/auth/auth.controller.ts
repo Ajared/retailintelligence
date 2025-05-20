e084695369a010fa029587c3c1c827bf5585be0e
prudentbird <prudentbird@gmail.com>
2025-05-20 23:44:43 +0100

    feat(auth): implement invite functionality and enhance authentication methods
    - Added invite method to send invitation emails with user roles.
    - Updated register and googleAuth methods to accept invite tokens from request headers.
    - Enhanced tests to cover new invite functionality and error handling for authentication methods.
    - Removed deprecated requestEmailVerification and verifyEmail methods to streamline the controller.

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -2,31 +2,44 @@ import { Throttle } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { SkipAuth } from '~/decorators/skip-auth.decorator';
 import {
+  Req,
   Post,
   Body,
-  Controller,
   HttpCode,
+  Controller,
   HttpStatus,
-  Query,
-  Get,
+  UseGuards,
 } from '@nestjs/common';
 import {
   AuthDto,
-  ForgotPasswordDto,
-  RequestEmailVerificationDto,
   ResetPasswordDto,
-  VerifyEmailDto,
+  ForgotPasswordDto,
+  GoogleAuthDto,
+  SendInviteEmailDto,
 } from './dto/auth-request.dto';
+import { Request } from 'express';
+import { AuthGuard } from '~/guards/auth.guard';
+import { RoleGuard } from '~/guards/role.guard';
+import { Roles } from '~/decorators/role.decorator';
+import { UserRole } from '~/modules/user/constants/user.constant';
 
 @SkipAuth()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('invite')
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
+  invite(@Body() sendInviteEmailDto: SendInviteEmailDto) {
+    return this.authService.sendInviteEmail(sendInviteEmailDto);
+  }
+
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  register(@Body() authDto: AuthDto) {
-    return this.authService.register(authDto);
+  register(@Body() authDto: AuthDto, @Req() req: Request) {
+    return this.authService.register(authDto, req.headers.inviteToken);
   }
 
   @Post('login')
@@ -48,20 +61,9 @@ export class AuthController {
     return this.authService.resetPassword(resetPasswordDto);
   }
 
-  @Post('verify-email')
+  @Post('google')
   @HttpCode(HttpStatus.OK)
-  @Throttle({ default: { limit: 3, ttl: 3600000 } })
-  requestEmailVerification(
-    @Body() requestEmailVerificationDto: RequestEmailVerificationDto,
-  ) {
-    return this.authService.requestEmailVerification(
-      requestEmailVerificationDto,
-    );
-  }
-
-  @Get('verify-email')
-  @HttpCode(HttpStatus.OK)
-  verifyEmail(@Query() verifyEmailDto: VerifyEmailDto) {
-    return this.authService.verifyEmail(verifyEmailDto);
+  googleAuth(@Body() googleAuthDto: GoogleAuthDto, @Req() req: Request) {
+    return this.authService.googleAuth(googleAuthDto, req.headers.inviteToken);
   }
 }
