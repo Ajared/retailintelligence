db4bcf1b5f45c5bb312b76e8c4ac908428c28a58
prudentbird <prudentbird@gmail.com>
2025-05-20 23:42:17 +0100

    refactor(seeder): update user seeder to utilize UserRole and UserStatus enums
    - Modified AppSeeder to check for SUPER_ADMIN role instead of isSuperAdmin flag.
    - Updated UserFactoryClass to set user role to SUPER_ADMIN and status to ACTIVE during user creation.

--- backend/src/database/seeder/app.seeder.ts
+++ backend/src/database/seeder/app.seeder.ts
@@ -3,7 +3,7 @@ import { Logger } from '@nestjs/common';
 import { trySafe } from '~/helpers/try-safe';
 import { User } from '~/modules/user/entities/user.entity';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
-
+import { UserRole } from '~/modules/user/constants/user.constant';
 export class AppSeeder implements Seeder {
   public async run(
     datasource: DataSource,
@@ -15,7 +15,9 @@ export class AppSeeder implements Seeder {
     const [error] = await trySafe(async () => {
       await datasource.transaction(async () => {
         if (
-          !(await userRepository.findOne({ where: { isSuperAdmin: true } }))
+          !(await userRepository.findOne({
+            where: { role: UserRole.SUPER_ADMIN },
+          }))
         ) {
           await factoryManager.get(User).save();
           logger.log(`Super Admin successfully seeded`);
