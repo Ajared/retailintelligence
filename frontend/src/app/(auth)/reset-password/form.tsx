67ddfd4e9545dfd810510fe265ff85e945945b1b
prudentbird <prudentbird@gmail.com>
2025-05-27 22:06:38 +0100

    feat(auth): implement forgot and reset password functionality
    - Added actions for handling forgot and reset password processes, including form validation using Zod.
    - Created new forms for forgot and reset password, integrating OTP input for enhanced security.
    - Updated schema to include validation for forgot and reset password forms.
    - Enhanced user experience with appropriate error handling and messaging in forms.

--- frontend/src/app/(auth)/reset-password/form.tsx
+++ frontend/src/app/(auth)/reset-password/form.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import type React from 'react';
-
 import {
   Card,
   CardContent,
@@ -10,71 +8,56 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card';
-import { registerUser } from '../actions';
+import Link from 'next/link';
 import { Response } from '~/types/actions';
-import { useRouter } from 'next/navigation';
-import { UserInterface } from '~/types/user';
-import { RegisterFormData } from '../schema';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
 import { Button } from '~/components/ui/button';
 import { useActionState, useState } from 'react';
+import { resetPasswordAction } from '../actions';
+import { ResetPasswordFormData } from '../schema';
 import { Alert, AlertDescription } from '~/components/ui/alert';
 import { CheckCircle2, Eye, EyeOff, Loader2 } from 'lucide-react';
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+} from '~/components/ui/input-otp';
+import { InputOTPSlot } from '~/components/ui/input-otp';
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from 'input-otp';
+import { UserInterface } from '~/types/user';
 
-export function RegisterForm({
-  email,
-  inviteToken,
-  isValidToken,
-}: {
-  email: string;
-  inviteToken: string;
-  isValidToken: boolean;
-}) {
-  const router = useRouter();
+export function ResetPasswordForm() {
   const [showPassword, setShowPassword] = useState(false);
 
-  const initialState: Response<UserInterface> & { inputs: RegisterFormData } = {
+  const initialState: Response<UserInterface> & {
+    inputs: ResetPasswordFormData;
+  } = {
     inputs: {
       email: '',
       password: '',
       confirmPassword: '',
-      inviteToken: '',
+      token: '',
     },
     error: '',
     message: '',
     timestamp: '',
   };
 
-  const [state, action, isPending] = useActionState(registerUser, initialState);
-
-  if (!isValidToken) {
-    return (
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle>Invalid Invitation</CardTitle>
-          <CardDescription>
-            The invitation token is invalid or has expired.
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <Button onClick={() => router.push('/login')} className="w-full">
-            Login
-          </Button>
-        </CardFooter>
-      </Card>
-    );
-  }
+  const [state, action, isPending] = useActionState(
+    resetPasswordAction,
+    initialState,
+  );
 
   return (
     <Card className="w-full max-w-md">
       <CardHeader>
-        <CardTitle>Create an account</CardTitle>
+        <CardTitle>Reset Password</CardTitle>
         <CardDescription>
-          Enter your password to get started with your account
+          Enter your new password to reset your account.
         </CardDescription>
       </CardHeader>
-      <form action={action} autoComplete="on" className="flex flex-col gap-5">
+      <form action={action} autoComplete="on" className="flex flex-col gap-4">
         <CardContent className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
@@ -83,42 +66,33 @@ export function RegisterForm({
               name="email"
               type="email"
               placeholder="name@example.com"
-              defaultValue={
-                email || ('inputs' in state && state.inputs?.email) || ''
-              }
-              readOnly={!!email}
+              defaultValue={('inputs' in state && state.inputs?.email) || ''}
               required
-              tabIndex={-1}
-              style={{ userSelect: 'none' }}
-              onFocus={(e) => e.target.blur()}
             />
           </div>
+
           <div className="space-y-2">
             <Label htmlFor="password">Password</Label>
-            <div className="relative">
-              <Input
-                id="password"
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                placeholder="••••••••"
-                required
-                minLength={6}
-                defaultValue={
-                  ('inputs' in state && state.inputs?.password) || ''
-                }
-              />
-            </div>
+            <Input
+              required
+              id="password"
+              minLength={6}
+              name="password"
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Enter your new password"
+              defaultValue={('inputs' in state && state.inputs?.password) || ''}
+            />
           </div>
           <div className="space-y-2">
             <Label htmlFor="confirmPassword">Confirm Password</Label>
             <div className="relative">
               <Input
+                required
                 id="confirmPassword"
+                minLength={6}
                 name="confirmPassword"
                 type={showPassword ? 'text' : 'password'}
-                placeholder="••••••••"
-                required
-                minLength={6}
+                placeholder="Confirm your new password"
                 defaultValue={
                   ('inputs' in state && state.inputs?.confirmPassword) || ''
                 }
@@ -138,15 +112,32 @@ export function RegisterForm({
             </div>
           </div>
 
-          <input
-            type="hidden"
-            name="inviteToken"
-            defaultValue={
-              inviteToken ||
-              ('inputs' in state && state.inputs?.inviteToken) ||
-              ''
-            }
-          />
+          <div className="space-y-2">
+            <Label htmlFor="token">One-Time Password</Label>
+            <InputOTP
+              required
+              id="token"
+              name="token"
+              maxLength={8}
+              className="w-full"
+              pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+              // value={('inputs' in state && state.inputs?.token) || ''}
+            >
+              <InputOTPGroup className="w-full">
+                <InputOTPSlot className="w-full h-12" index={0} />
+                <InputOTPSlot className="w-full h-12" index={1} />
+                <InputOTPSlot className="w-full h-12" index={2} />
+                <InputOTPSlot className="w-full h-12" index={3} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup className="w-full">
+                <InputOTPSlot className="w-full h-12" index={4} />
+                <InputOTPSlot className="w-full h-12" index={5} />
+                <InputOTPSlot className="w-full h-12" index={6} />
+                <InputOTPSlot className="w-full h-12" index={7} />
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
 
           {state?.message && (
             <Alert variant={'data' in state ? 'default' : 'destructive'}>
@@ -164,24 +155,24 @@ export function RegisterForm({
             </Alert>
           )}
         </CardContent>
-
-        <CardFooter className="flex flex-col space-y-4">
+        <CardFooter className="flex flex-col gap-2">
           <Button
             type="submit"
             className="w-full cursor-pointer"
             disabled={isPending}
           >
             {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            {isPending ? 'Processing' : 'Create Account'}
+            {isPending ? 'Processing' : 'Change Password'}
           </Button>
           <div className="text-center text-sm">
-            Already have an account?{' '}
+            Remember your password?{' '}
             <Button
+              asChild
+              type="button"
               variant="link"
               className="px-0 cursor-pointer"
-              onClick={() => router.push('/login')}
             >
-              Sign In
+              <Link href="/login">Login Here</Link>
             </Button>
           </div>
         </CardFooter>
