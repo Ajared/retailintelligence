b8657c130823cfb3177e3d06530a62fb89c83966
prudentbird <prudentbird@gmail.com>
2025-05-27 02:47:34 +0100

    chore: project initialization

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,9 @@
-import type { NextConfig } from "next";
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import './src/env.ts';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
