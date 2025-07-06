8fcc1256660814afea3acc0c5f82af6e27135716
prudentbird <prudentbird@gmail.com>
2025-07-06 15:20:14 +0100

    feat: add location assignment columns and relationships to user and related entities
    - Created a migration to add columns for assigned state, local government, phase, and district to the users table.
    - Established foreign key relationships for the new columns with the corresponding entities (states, local governments, phases, and districts).
    - Updated user, state, local government, district, and phase entities to include relationships to users for better data management and retrieval.

--- backend/src/modules/local-government/entities/local-government.entity.ts
+++ backend/src/modules/local-government/entities/local-government.entity.ts
@@ -1,14 +1,15 @@
+import {
+  Index,
+  Entity,
+  Column,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '~/modules/user/entities/user.entity';
 import { Store } from '~/modules/store/entities/store.entity';
 import { State } from '~/modules/state/entities/state.entity';
 import { AbstractBaseEntity } from '~/database/base/base.entity';
-import {
-  Entity,
-  OneToMany,
-  Column,
-  ManyToOne,
-  JoinColumn,
-  Index,
-} from 'typeorm';
 
 @Entity({ name: 'local_governments' })
 export class LocalGovernment extends AbstractBaseEntity {
@@ -23,4 +24,9 @@ export class LocalGovernment extends AbstractBaseEntity {
   stateId: string;
   @OneToMany(() => Store, (store) => store.localGovernment)
   stores: Store[];
+
+  @OneToMany(() => User, (user) => user.assignedLocalGovernment, {
+    nullable: true,
+  })
+  users?: User[];
 }
